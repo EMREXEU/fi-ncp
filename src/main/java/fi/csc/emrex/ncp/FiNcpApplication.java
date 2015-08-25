5565844d301261f97eb38c76b191904ecf355429
Marko Hollanti <mhollanti@gmail.com>
2015-08-25 13:09:11 +0300

    Initial commit

---
+++ src/main/java/fi/csc/emrex/ncp/FiNcpApplication.java
@@ -0,0 +1,12 @@
+package fi.csc.emrex.ncp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiNcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiNcpApplication.class, args);
+    }
+}
