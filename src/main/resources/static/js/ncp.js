2b0164b9079264a63ea34602dbe46ab1dbc89c69
Juho Pentikäinen <juho.pentikainen@gofore.com>
2015-09-17 15:13:00 +0300

    cleaned code a bit. added button for send ids

--- src/main/resources/static/js/ncp.js
+++ src/main/resources/static/js/ncp.js
@@ -1,80 +1,4 @@
-app = angular.module('fi-ncp', ['ngRoute'])
-    .directive('learningOpportunityCollection', function () {
-        return {
-            restrict: "E",
-            replace: true,
-            scope: {
-                collection: '=',
-                addIdFn: '&',
-                removeIdFn: '&'
-            },
-            controller: function($scope) {
-                console.log('COLLECTION ' + JSON.stringify($scope.collection));
-            },
-            templateUrl: "partials/learning-opportunity-collection.html",
-            link: function (scope, elm, attrs) {
-                scope.addId = function(id) {
-                    console.log('add id in COLLECTION link ' + id);
-                    scope.addIdFn({id : id})
-                }
-
-                scope.removeId = function(id) {
-                    console.log('remove id in COLLECTION link ' + id);
-                    scope.removeIdFn({id : id})
-                }
-            }
-        }
-    })
-    .directive('learningOpportunity', function ($compile) {
-        return {
-            restrict: "E",
-            replace: true,
-            scope: {
-                member: '=',
-                addIdFn: '&',
-                removeIdFn: '&'
-            },
-            controller: function ($scope) {
-
-                console.log('MEMBER ' + JSON.stringify($scope.member));
-                angular.forEach($scope.member.identifier, function (identifier) {
-                    if (identifier.type == "elmo")
-                        $scope.elmoIdentifier = identifier.content;
-                });
-
-                $scope.checkBoxChanged = function () {
-                    console.log($scope.checked + ' ' + $scope.elmoIdentifier);
-                    if ($scope.checked)
-                        $scope.addId({id: $scope.elmoIdentifier})
-                    else
-                        $scope.removeId({id: $scope.elmoIdentifier});
-                };
-
-
-            },
-            templateUrl: "partials/learning-opportunity.html",
-            link: function (scope, element, attrs) {
-
-                if (angular.isArray(scope.member.hasPart)) {
-                    element.append("<learning-opportunity-collection add-id-fn='addId(id)' remove-id-fn='removeId(id)' collection='member.hasPart'></learning-opportunity-collection>");
-                    $compile(element.contents())(scope)
-                }
-
-                scope.addId = function (id) {
-                    console.log('add id in MEMBER ' + id);
-                    scope.addIdFn({id: id})
-                }
-
-
-                scope.removeId = function (id) {
-                    console.log('remove id in MEMBER link ' + id);
-                    scope.removeIdFn({id: id})
-
-                }
-            }
-
-        }
-    })
+app = angular.module('fi-ncp', ['ngRoute', 'learningOpportunityRecursion'])
 
 app.config(function ($routeProvider, $httpProvider) {
 
@@ -115,25 +39,41 @@ app.config(function ($routeProvider, $httpProvider) {
 })
 ;
 
-app.controller('courseSelection', function($scope, response) {
+app.controller('courseSelection', function ($scope, response) {
     var report = response.data.elmo.report;
+
+    // learningOpportunity must be an array for working recursion..
     if (!angular.isArray(report.learningOpportunitySpecification))
-        report.learningOpportunitySpecification = [{learningOpportunitySpecification : report.learningOpportunitySpecification}];
+        report.learningOpportunitySpecification = [{learningOpportunitySpecification: report.learningOpportunitySpecification}];
     $scope.learningOpportunities = report.learningOpportunitySpecification;
+
+    console.log($scope.learningOpportunities);
+
     $scope.selectedIds = [];
 
-    $scope.addId = function(id) {
+    $scope.addId = function (id) {
         console.log('add id ' + id.id);
         if ($scope.selectedIds.indexOf(id.id) < 0)
             $scope.selectedIds.push(id.id);
     };
 
-    $scope.removeId = function(id) {
+    $scope.removeId = function (id) {
         console.log('remove id ' + id.id);
         var index = $scope.selectedIds.indexOf(id.id);
         console.log(index)
         if (index >= 0)
-            $scope.selectedIds.splice(index,1);
+            $scope.selectedIds.splice(index, 1);
+    };
+
+
+    $scope.sendIds = function () {
+        $http({
+            url: 'api/review',
+            method: 'GET',
+            params: selectedIds
+        }).success(function (data) {
+            console.log(data);
+        });
     };
 });
 
