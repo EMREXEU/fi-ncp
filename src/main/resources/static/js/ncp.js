e96326c0a4ff81fe114f5922b53c23e817fd8712
Juho Pentikäinen <juho.pentikainen@gofore.com>
2015-09-17 14:19:57 +0300

    recursion with callbacks working. needs cleaning

--- src/main/resources/static/js/ncp.js
+++ src/main/resources/static/js/ncp.js
@@ -4,12 +4,25 @@ app = angular.module('fi-ncp', ['ngRoute'])
             restrict: "E",
             replace: true,
             scope: {
-                collection: '='
+                collection: '=',
+                addIdFn: '&',
+                removeIdFn: '&'
             },
             controller: function($scope) {
                 console.log('COLLECTION ' + JSON.stringify($scope.collection));
             },
-            templateUrl: "partials/learning-opportunity-collection.html"
+            templateUrl: "partials/learning-opportunity-collection.html",
+            link: function (scope, elm, attrs) {
+                scope.addId = function(id) {
+                    console.log('add id in COLLECTION link ' + id);
+                    scope.addIdFn({id : id})
+                }
+
+                scope.removeId = function(id) {
+                    console.log('remove id in COLLECTION link ' + id);
+                    scope.removeIdFn({id : id})
+                }
+            }
         }
     })
     .directive('learningOpportunity', function ($compile) {
@@ -17,18 +30,49 @@ app = angular.module('fi-ncp', ['ngRoute'])
             restrict: "E",
             replace: true,
             scope: {
-                member: '='
+                member: '=',
+                addIdFn: '&',
+                removeIdFn: '&'
             },
-            controller: function($scope) {
+            controller: function ($scope) {
+
                 console.log('MEMBER ' + JSON.stringify($scope.member));
+                angular.forEach($scope.member.identifier, function (identifier) {
+                    if (identifier.type == "elmo")
+                        $scope.elmoIdentifier = identifier.content;
+                });
+
+                $scope.checkBoxChanged = function () {
+                    console.log($scope.checked + ' ' + $scope.elmoIdentifier);
+                    if ($scope.checked)
+                        $scope.addId({id: $scope.elmoIdentifier})
+                    else
+                        $scope.removeId({id: $scope.elmoIdentifier});
+                };
+
+
             },
             templateUrl: "partials/learning-opportunity.html",
             link: function (scope, element, attrs) {
+
                 if (angular.isArray(scope.member.hasPart)) {
-                    element.append("<learning-opportunity-collection collection='member.hasPart'></learning-opportunity-collection>");
+                    element.append("<learning-opportunity-collection add-id-fn='addId(id)' remove-id-fn='removeId(id)' collection='member.hasPart'></learning-opportunity-collection>");
                     $compile(element.contents())(scope)
                 }
+
+                scope.addId = function (id) {
+                    console.log('add id in MEMBER ' + id);
+                    scope.addIdFn({id: id})
+                }
+
+
+                scope.removeId = function (id) {
+                    console.log('remove id in MEMBER link ' + id);
+                    scope.removeIdFn({id: id})
+
+                }
             }
+
         }
     })
 
@@ -76,6 +120,21 @@ app.controller('courseSelection', function($scope, response) {
     if (!angular.isArray(report.learningOpportunitySpecification))
         report.learningOpportunitySpecification = [{learningOpportunitySpecification : report.learningOpportunitySpecification}];
     $scope.learningOpportunities = report.learningOpportunitySpecification;
+    $scope.selectedIds = [];
+
+    $scope.addId = function(id) {
+        console.log('add id ' + id.id);
+        if ($scope.selectedIds.indexOf(id.id) < 0)
+            $scope.selectedIds.push(id.id);
+    };
+
+    $scope.removeId = function(id) {
+        console.log('remove id ' + id.id);
+        var index = $scope.selectedIds.indexOf(id.id);
+        console.log(index)
+        if (index >= 0)
+            $scope.selectedIds.splice(index,1);
+    };
 });
 
 app.controller('norex', function ($scope, $http) {
