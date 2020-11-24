6f2fdee290e5365ee0a7b5790b40ec2013ae1cdb
Tomi Tolvanen <Tomi.Tolvanen.Gofore@maakunta.cloud>
2020-11-24 09:24:50 +0200

    Refac session attributes.

--- src/test/java/fi/csc/emrex/ncp/ThymeControllerIntegrationTest.java
+++ src/test/java/fi/csc/emrex/ncp/ThymeControllerIntegrationTest.java
@@ -5,6 +5,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import fi.csc.emrex.ncp.controller.NcpPages;
 import fi.csc.emrex.ncp.controller.NcpSessionAttributes;
 import fi.csc.emrex.ncp.elmo.ElmoParser;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,7 +17,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -49,15 +49,7 @@ public class ThymeControllerIntegrationTest {
         .post("/ncp")
         .param("sessionId", "TODO")
         .param("returnUrl", "TODO")
-        .sessionAttr("SHIB_funetEduPersonLearnerId", "1.2.246.562.24.17488477125")
-        .sessionAttr("SHIB_schacDateOfBirth", "19660718")
-        .sessionAttr("SHIB_schacHomeOrganization", "oamk.fi")
-        .sessionAttr("cn", "Teppo Testääja")
-        .sessionAttr("displayName", "Kaisa")
-        .sessionAttr("givenName", "Kaisa")
-        .sessionAttr("sn", "Keränen")
-        .sessionAttr("unique-code", "urn:mace:terena.org:schac:personalUniqueCode:fi:oamk.fi:x8734")
-        .sessionAttr("unique-id", "urn:mace:terena.org:schac:personalUniqueID:fi:FIC:180766-2213"))
+        .sessionAttrs(getShibbolethAuthenticationAttributes()))
         .andDo(print())
         .andExpect(MockMvcResultMatchers.status().isOk())
         .andExpect(MockMvcResultMatchers.content().string(NcpPages.NOREX))
@@ -72,32 +64,12 @@ public class ThymeControllerIntegrationTest {
   @Test
   public void getCoursesAndReview() throws Exception {
 
-    // Shibboleth session params:
-
-    //SHIB_funetEduPersonLearnerId: 1.2.246.562.24.17488477125
-    //SHIB_schacDateOfBirth: 19660718
-    //SHIB_schacHomeOrganization: oamk.fi
-    //cn: Teppo Testääja
-    //displayName: Kaisa
-    //givenName: Kaisa
-    //sn: Keränen
-    //unique-code: urn:mace:terena.org:schac:personalUniqueCode:fi:oamk.fi:x8734
-    //unique-id: urn:mace:terena.org:schac:personalUniqueID:fi:FIC:180766-2213
-
     // First request will store data into session so must use same session in following request
     MvcResult res = mockMvc.perform(MockMvcRequestBuilders
         .post("/ncp")
         .param("sessionId", "TODO")
         .param("returnUrl", "TODO")
-        .sessionAttr("SHIB_funetEduPersonLearnerId", "1.2.246.562.24.17488477125")
-        .sessionAttr("SHIB_schacDateOfBirth", "19660718")
-        .sessionAttr("SHIB_schacHomeOrganization", "oamk.fi")
-        .sessionAttr("cn", "Teppo Testääja")
-        .sessionAttr("displayName", "Kaisa")
-        .sessionAttr("givenName", "Kaisa")
-        .sessionAttr("sn", "Keränen")
-        .sessionAttr("unique-code", "urn:mace:terena.org:schac:personalUniqueCode:fi:oamk.fi:x8734")
-        .sessionAttr("unique-id", "urn:mace:terena.org:schac:personalUniqueID:fi:FIC:180766-2213"))
+        .sessionAttrs(getShibbolethAuthenticationAttributes()))
         .andDo(print())
         .andExpect(MockMvcResultMatchers.status().isOk())
         .andExpect(MockMvcResultMatchers.content().string(NcpPages.NOREX))
@@ -117,16 +89,26 @@ public class ThymeControllerIntegrationTest {
         .andExpect(MockMvcResultMatchers.content().string(NcpPages.REVIEW));
   }
 
-  MockHttpServletRequestBuilder addShibbolethAuthentication(MockHttpServletRequestBuilder builder) {
-    return builder
-        .sessionAttr("SHIB_funetEduPersonLearnerId", "1.2.246.562.24.17488477125")
-        .sessionAttr("SHIB_schacDateOfBirth", "19660718")
-        .sessionAttr("SHIB_schacHomeOrganization", "oamk.fi")
-        .sessionAttr("cn", "Teppo Testääja")
-        .sessionAttr("displayName", "Kaisa")
-        .sessionAttr("givenName", "Kaisa")
-        .sessionAttr("sn", "Keränen")
-        .sessionAttr("unique-code", "urn:mace:terena.org:schac:personalUniqueCode:fi:oamk.fi:x8734")
-        .sessionAttr("unique-id", "urn:mace:terena.org:schac:personalUniqueID:fi:FIC:180766-2213");
+  //  Shibboleth session params:
+  //SHIB_funetEduPersonLearnerId: 1.2.246.562.24.17488477125
+  //SHIB_schacDateOfBirth: 19660718
+  //SHIB_schacHomeOrganization: oamk.fi
+  //cn: Teppo Testääja
+  //displayName: Kaisa
+  //givenName: Kaisa
+  //sn: Keränen
+  //unique-code: urn:mace:terena.org:schac:personalUniqueCode:fi:oamk.fi:x8734
+  //unique-id: urn:mace:terena.org:schac:personalUniqueID:fi:FIC:180766-2213
+  Map<String, Object> getShibbolethAuthenticationAttributes() {
+    return Map.of(
+        "SHIB_funetEduPersonLearnerId", "1.2.246.562.24.17488477125",
+        "SHIB_schacDateOfBirth", "19660718",
+        "SHIB_schacHomeOrganization", "oamk.fi",
+        "cn", "Teppo Testääja",
+        "displayName", "Kaisa",
+        "givenName", "Kaisa",
+        "sn", "Keränen",
+        "unique-code", "urn:mace:terena.org:schac:personalUniqueCode:fi:oamk.fi:x8734",
+        "unique-id", "urn:mace:terena.org:schac:personalUniqueID:fi:FIC:180766-2213");
   }
 }
