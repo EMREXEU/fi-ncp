c02656e8ea1ea1eed2ade75ebaf4c06ba98eff5e
Tomi Tolvanen <Tomi.Tolvanen.Gofore@maakunta.cloud>
2020-12-11 11:29:10 +0200

    Add tests.

--- src/test/java/fi/csc/emrex/ncp/util/FidUtilTest.java
+++ src/test/java/fi/csc/emrex/ncp/util/FidUtilTest.java
@@ -9,9 +9,33 @@ import org.junit.jupiter.api.Test;
 public class FidUtilTest {
 
   @Test
-  public void parseFid() throws NpcException {
+  public void parseFidFromBday() throws NpcException {
     String shibBday = "19660718";
+    String shibUid = "";
+    OpintosuorituksetResponse virtaXml = null;
+    XMLGregorianCalendar cal = FidUtil.resolveBirthDate(shibBday, shibUid, virtaXml);
+
+    Assertions.assertEquals(1966, cal.getYear());
+    Assertions.assertEquals(7, cal.getMonth());
+    Assertions.assertEquals(18, cal.getDay());
+  }
+
+  @Test
+  public void parseFidFromUid() throws NpcException {
+    String shibBday = "";
     String shibUid = "urn:mace:terena.org:schac:personalUniqueID:fi:FIC:180766-2213";
+    OpintosuorituksetResponse virtaXml = null;
+    XMLGregorianCalendar cal = FidUtil.resolveBirthDate(shibBday, shibUid, virtaXml);
+
+    Assertions.assertEquals(1966, cal.getYear());
+    Assertions.assertEquals(7, cal.getMonth());
+    Assertions.assertEquals(18, cal.getDay());
+  }
+
+  @Test
+  public void parseFidFromVirtaXml() throws NpcException {
+    String shibBday = "";
+    String shibUid = "";
     OpintosuorituksetResponse virtaXml = new OpintosuorituksetResponse();
     XMLGregorianCalendar cal = FidUtil.resolveBirthDate(shibBday, shibUid, virtaXml);
 
