b4e2164eb4e30a0a8958c611d57a662a1e246bd3
Tomi Tolvanen <Tomi.Tolvanen.Gofore@maakunta.cloud>
2021-01-11 17:04:36 +0200

    Handle course selection in set. Map organization code to dummy name.

--- src/main/fi-ncp-front/src/app/app.component.ts
+++ src/main/fi-ncp-front/src/app/app.component.ts
@@ -10,13 +10,17 @@ export class AppComponent {
   title = 'fi-ncp-front';
   plainTextRes: any;
   jsonRes: any;
-  selectedCourses: Array<any>;
+  selectedCourses: Set<any>;
+  organizationNames: Map<any, any>;
 
 
   constructor(private httpClient: HttpClient) {
 
     const url = 'http://localhost:9001/test/ncp/?sessionId=1234&returnUrl=234123&unique-id=urn:mace:terena.org:schac:personalUniqueID:fi:FIC:180766-2213&SHIB_funetEduPersonLearnerId=1.2.246.562.24.17488477125&SHIB_schacHomeOrganization=oamk.fi&SHIB_schacHomeOrganizationId=02536';
-    this.selectedCourses = [];
+    this.selectedCourses = new Set();
+    this.organizationNames = new Map<any, any>();
+    // TODO: fetch real mapping
+    this.organizationNames.set("02536", "Testikoulu");
 
     // Default response type: JSON
     httpClient.get(url, {responseType: 'text'}).subscribe(res => {
@@ -35,13 +39,21 @@ export class AppComponent {
     // TODO: add selected course / remove deselected...
     // TODO: Update checkbox
     // TODO: Use map (no dupplicates)
-    console.log("Course selected:" + x);
-    this.selectedCourses.push(x);
+
+    if (this.selectedCourses.has(x)) {
+      this.selectedCourses.delete(x);
+      console.log("Removed course:" + x);
+    } else {
+      console.log("Added course:" + x);
+      this.selectedCourses.add(x);
+    }
+
+    console.log("Courses to review:" + Array.from(this.selectedCourses.values()).toString());
 
   }
 
   reviewSelectedCourses() {
-    window.alert("Valitsit kurssit:" + this.selectedCourses.toString());
-    console.log("Courses to review:" + this.selectedCourses.toString());
+    console.log("Courses to review:" + Array.from(this.selectedCourses.values()).toString());
+    window.alert("Valitsit kurssit:" + Array.from(this.selectedCourses.values()).toString());
   }
 }
