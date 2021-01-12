ebd3126ceee206ac8b383c275026e5be7ca3d302
Tomi Tolvanen <Tomi.Tolvanen.Gofore@maakunta.cloud>
2021-01-12 09:24:57 +0200

    Add material UI.

--- src/main/fi-ncp-front/src/app/app.module.ts
+++ src/main/fi-ncp-front/src/app/app.module.ts
@@ -4,6 +4,11 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatPseudoCheckboxModule} from "@angular/material/core";
+import {MatTooltipModule} from "@angular/material/tooltip";
+
 
 @NgModule({
   declarations: [
@@ -12,7 +17,11 @@ import {HttpClientModule} from "@angular/common/http";
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatSliderModule,
+    MatPseudoCheckboxModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
