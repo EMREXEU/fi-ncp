2978ea6c4cb1989b1c18e5471c519f306b1d9444
Tomi Tolvanen <Tomi.Tolvanen.Gofore@maakunta.cloud>
2021-01-20 11:20:20 +0200

    update URIs.

--- src/test/resources/scripts/localhost-test-ncp-review.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl --location --request GET 'http://localhost:9001/test/ncp_review/?sessionId=1234&returnUrl=234123&unique-id=urn:mace:terena.org:schac:personalUniqueID:fi:FIC:180766-2213&SHIB_funetEduPersonLearnerId=1.2.246.562.24.17488477125&SHIB_schacHomeOrganization=oamk.fi&SHIB_schacHomeOrganizationId=02536' \
---header 'Cookie: JSESSIONID=63509369F8C576D9718C3C8C1F69D6AB' \
---data-raw '' \
---verbose
